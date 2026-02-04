0c1dc5ffa9beedd18c982675743a5c7654439f19
deterius <bourim@gmail.com>
2026-02-04 10:46:46 +0800

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Simeon Bourim on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
