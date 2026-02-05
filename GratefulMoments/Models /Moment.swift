59d2e8266e25c0f77813f2f726bd1704e7764d77
deterius <bourim@gmail.com>
2026-02-05 10:54:24 +0800

    all files

---
+++ GratefulMoments/Models /Moment.swift
@@ -0,0 +1,8 @@
+//
+//  Moment.swift
+//  GratefulMoments
+//
+//  Created by Simeon Bourim on 2/5/26.
+//
+
+import Foundation
---
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by Simeon Bourim on 2/4/26.
+//
+
+import SwiftUI
+
+struct MomentEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentEntryView()
+}
