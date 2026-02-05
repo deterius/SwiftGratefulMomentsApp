aa9a32909db1f35a0395ba2577c74112e2dcf8ca
deterius <bourim@gmail.com>
2026-02-05 11:06:31 +0800

    commit

--- GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -1,17 +1,17 @@
-//
-//  GratefulMomentsApp.swift
-//  GratefulMoments
-//
-//  Created by Simeon Bourim on 2/4/26.
-//
-
 import SwiftUI
+import SwiftData
+
 
 @main
 struct GratefulMomentsApp: App {
+    let dataContainer = DataContainer()
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContainer)
         }
+        .modelContainer(dataContainer.modelContainer)
     }
 }
